353523b564fa3a4cd9f0d259cc42d73ea7ac45af
cole-wahlers1 <79235390+cole-wahlers1@users.noreply.github.com>
2021-09-19 14:36:08 -0400

    Add files via upload

---
+++ 1-4/3.6.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def index():
+ return '<h1>Hello World!</h1>'
+@app.route('/user/<name>')
+def user(name):
+ return '<h1>Hello, %s!</h1>' % name
+if __name__ == '__main__':
+ app.run(debug=True)
+
+@app.errorhandler(404)
+def page_not_found(e):
+ return render_template('404.html'), 404
+@app.errorhandler(500)
+def internal_server_error(e):
+ return render_template('500.html'), 500
